e5a822d7a99900a07355606ceb9d7563d98df0c2
Harrinson Q <jhaqual@gmail.com>
2021-04-22 14:07:53 -0400

    proyectCrudInitial

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::get('/', 'UserController@index');
+Route::post('users', 'UserController@store')->name('users.store');
+Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
+
+
+
+// Otra forma de configurar los controladores
+/*
+/ use App\Http\Controllers\UserController;
+/ Route::get('/', [UserController::class,'index']);
+/ Route::post('users', [UserController::class, 'store'])->name('users.store');
+/ Route::delete('users/{user}', [UserController::class,'destroy'])->name('users.destroy');
+/
+*/
